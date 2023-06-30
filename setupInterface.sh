101d9890f08bc60305969daa2cc72a1db7e28d3b
diego <diego.giuseppetti@gmail.com>
2023-06-30 17:05:38 +0200

    Final script

--- setupInterface.sh
+++ setupInterface.sh
@@ -1,8 +1,7 @@
 sudo brctl addbr virbr0
 sudo brctl stp virbr0 on
-sudo ip address add 192.168.10.1 dev virbr0 broadcast 192.168.122.255
+sudo ip addr add 192.168.122.0/24 dev virbr0 broadcast 192.168.122.255
 sudo ip link set virbr0 up
+sudo ip route add 192.168.122.2 via 192.168.122.2 dev virbr0
 
 #reindirizzamento browser verso interfaccia server proxmox
-sudo ip addr add 192.168.122.1/24 dev virbr0 broadcast 192.168.122.255
-sudo ip addr add 192.168.122.2/24 dev virbr0 broadcast 192.168.122.255
